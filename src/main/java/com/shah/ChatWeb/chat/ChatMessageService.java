a081f2ed81b3406528492fe34c83f248f74d6103
Khizarshah01 <khizar.shah010@gmail.com>
2024-11-21 20:29:08 +0530

    Initial commit

---
+++ src/main/java/com/shah/ChatWeb/chat/ChatMessageService.java
@@ -0,0 +1,31 @@
+package com.shah.ChatWeb.chat;
+
+import com.shah.ChatWeb.chatroom.ChatRoomRepository;
+import com.shah.ChatWeb.chatroom.ChatRoomService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ChatMessageService {
+    private final ChatMessageRepository repository;
+    private final ChatRoomService chatRoomService;
+
+    public ChatMessage save(ChatMessage chatMessage){
+        var chatId = chatRoomService.getChatRoomId(chatMessage.getSenderId(),chatMessage.getRecipientId(),true)
+                .orElseThrow(); // You can throw you want
+        chatMessage.setChatId(chatId);
+        repository.save(chatMessage);
+        return chatMessage;
+    }
+
+    public List<ChatMessage> findChatMessages(
+            String senderId, String recipientId
+    ){
+        var chatId = chatRoomService.getChatRoomId(senderId,recipientId,false);
+        return chatId.map(repository::findByChatId).orElse(new ArrayList<>());
+    }
+}
---
+++ src/main/java/com/shah/ChatWeb/user/Status.java
@@ -0,0 +1,6 @@
+package com.shah.ChatWeb.user;
+
+// Define the Status enum inside the class
+public enum Status {
+    ONLINE, OFFLINE
+}
